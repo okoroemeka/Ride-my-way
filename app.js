0d60b4a2eae817ab54de8078dec36a7421e7dd15
okoro emeka <solomonokoro056@gmail.com>
2018-06-19 18:03:17 +0100

    Add get all ride offers rout

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import rideOffersController from './server/controller/rideOfferControllers';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the begining of nothingness',
-}));
+const store = {
+  ridesOffer: [{
+    firstName: 'emeka',
+    lastName: 'okoro',
+    phoneNumber: 23408945976,
+    currentLocation: 'Ikeja',
+    destination: 'Ajaa',
+    DepartureTime: '12:00pm',
+    rideRequest: [],
+  },
+  ],
+};
+
+app.use((req, res, next) => {
+  req.store = store;
+  next();
+});
+
+app.get('/api/v1/rides', rideOffersController.getAllRideOffers);
+// app.get('/api/v1/rides/:rideOfferId', rideOffersController.getSpecificRideOffers);
+// app.post('/api/v1/rides', rideOffersController.creatRideOffer);
 
 export default app;
