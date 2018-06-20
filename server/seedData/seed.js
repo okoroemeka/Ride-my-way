ca12e799d508437b9d164a568cd1d516588718dc
okoro emeka <solomonokoro056@gmail.com>
2018-06-20 21:09:48 +0100

    Add user dummy database and seed data

---
+++ server/seedData/seed.js
@@ -0,0 +1,19 @@
+const store = {
+  ridesOffer: [{
+    firstName: 'emeka',
+    lastName: 'okoro',
+    phoneNumber: 23408945976,
+    currentLocation: 'Ikeja',
+    destination: 'Ajaa',
+    DepartureTime: '12:00pm',
+    rideRequests: [],
+  },
+  ],
+  user: [{
+    firstName: 'emeka',
+    lastName: 'okoro',
+    email: 'okoro@gmail.com',
+    password: 'wise2020',
+  }],
+};
+export default store;
