ddb184e0aeec86fc4c0140af56a52f032ff35d33
okoro emeka <solomonokoro056@gmail.com>
2018-06-20 21:03:10 +0100

    Add route for user sign in

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+
+// import seed data
+import store from './server/seedData/seed';
+
+// Importing controllers
 import rideOfferAuth from './server/middlewares/rideOfferAuth';
 import rideOffersController from './server/controller/rideOfferControllers';
 import userController from './server/controller/signupController';
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
-const store = {
-  ridesOffer: [{
-    firstName: 'emeka',
-    lastName: 'okoro',
-    phoneNumber: 23408945976,
-    currentLocation: 'Ikeja',
-    destination: 'Ajaa',
-    DepartureTime: '12:00pm',
-    rideRequests: [],
-  },
-  ],
-  user: [{
-    firstName: 'emeka',
-    lastName: 'okoro',
-    email: 'okoro@gmail.com',
-    password: 'wise2020',
-  }],
-};
-
+// Attaching dummy database to the req object
 app.use((req, res, next) => {
   req.store = store;
   next();
 });
 
+// Adding routes to controller(Routing)
 app.post('/api/v1/auth/signup', userController.userSignup);
+app.post('/api/v1/auth/signin', userController.userSignIn);
 app.get('/api/v1/rides', rideOffersController.getAllRideOffers);
 app.get('/api/v1/rides/:rideOfferId', rideOffersController.getSpecificRideOffers);
 app.post('/api/v1/rides', rideOffersController.creatRideOffer);
