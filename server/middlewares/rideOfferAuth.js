2e2d0d31201f09ee3316157c16f455302d818b95
okoro emeka <solomonokoro056@gmail.com>
2018-06-20 13:32:49 +0100

    Add method to check if ride offer exist

---
+++ server/middlewares/rideOfferAuth.js
@@ -0,0 +1,19 @@
+class RideOfferAuth {
+/**
+  *@returns{object}description
+  *@param {*} request
+  *@param {*} response
+  *@param {*} next
+*/
+  static rideOfferExist(req, res, next) {
+    const offerId = req.params.rideOfferId;
+    const rideOffer = req.store.ridesOffer[offerId];
+    if (!rideOffer) {
+      return res.status(404).send({
+        message: 'Ride offer does not exist',
+      });
+    }
+    return next();
+  }
+}
+export default RideOfferAuth;
