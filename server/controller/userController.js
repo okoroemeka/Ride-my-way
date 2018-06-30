ad0b206e329d74c6e6c7c54894900da6621b1f56
okoro emeka <solomonokoro056@gmail.com>
2018-06-30 19:37:19 +0100

    fix(userController):modify user controller
    - modify user controller after splitting seed data
    
    [Fixes #158745886]

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,5 @@
 /*User signin and sign up controller object*/
+import userSeedData from '../seedData/userSeedData';
 
 class User {
   /**
@@ -8,17 +9,18 @@ class User {
    */
   static userSignup(req, res) {
     const newUserEmail = req.body.email;
-    const newuserId = req.store.user.length;
+    const newUserId = userSeedData.length + 1;
 
     // new user details
     const newUserDetails = {
+      id: newUserId,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       password: req.body.password,
     };
 
-    const userAlreadyExist = req.store.user.filter(user => user.email === newUserEmail);
+    const userAlreadyExist = userSeedData.filter(user => user.email === newUserEmail);
 
     // checking if user already exists
     if (userAlreadyExist.length !== 0) {
@@ -26,30 +28,21 @@ class User {
         message: 'user already exists',
       });
     }
-    //  checking if the passwords match
-    if (req.body.password !== req.body.confirmPassword) {
-      return res.status(400).send({
-        message: 'The passwords do not match, please type them again',
-      });
-    }
-
-    // creating new user
-    req.store.user.push(newUserDetails);
+    userSeedData.push(newUserDetails);
     return res.status(201).send({
-      message: 'Welcome to Ride my way',
-      userId: newuserId,
+      message: 'Welome to ride my way'
     });
   }
 
   static userSignIn(req, res) {
     // get user information
-    const userInformation = {
-      email: req.body.email,
-      password: req.body.password,
-    };
+    // const userInformation = {
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // };
 
     // checking if user does not exist
-    const userExist = req.store.user
+    const userExist = userSeedData
       .filter(user => user.email === req.body.email && user.password === req.body.password);
 
     if (userExist.length !== 0) {
