6ff29159893839f3712c9e69a046126c4ddb9a0d
okoro emeka <solomonokoro056@gmail.com>
2018-06-30 16:17:18 +0100

    fix(seedData):split seed data to follow SRP
    - remove ride offer sedd data from seed.js
    
    add ride offer seed data to rideOfferSeedData.js
    
    [Fixes #158738997]

---
+++ server/seedData/rideOfferSeedData.js
@@ -0,0 +1,10 @@
+// Seed data for test
+const rideOffer = [{
+  firstName: 'emeka',
+  lastName: 'okoro',
+  phoneNumber: 23408945976,
+  currentLocation: 'ikeja',
+  destination: 'ajaa',
+  DepartureTime: '12:00pm',
+}];
+export default rideOffer;
