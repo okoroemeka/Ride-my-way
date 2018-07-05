0be4ae99fc979dbe61e955d4ac28bfbd451c2893
okoro emeka <solomonokoro056@gmail.com>
2018-07-05 11:23:46 +0100

    add route for create ride offer

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,19 +1,20 @@
 // Import controllers
-import rideOffersController from '../controller/rideOfferControllers';
-import rideOfferAuth from '../middlewares/rideOfferAuth';
-import userController from '../controller/userController';
+import rideOffers from '../controller/rideOffers';
+// import rideOfferAuth from '../middlewares/rideOfferAuth';
+
+import user from '../controller/user';
 
 // Adding routes to controller(Routing)
 
 const routes = (app) => {
-  app.post('/api/v1/auth/signup', userController.userSignup);
-  app.post('/api/v1/auth/signin', userController.userSignIn);
-  app.get('/api/v1/rides', rideOffersController.getAllRideOffers);
-  app.get('/api/v1/rides/:rideOfferId', rideOffersController.getSpecificRideOffer);
-  app.post('/api/v1/rides', rideOffersController.creatRideOffer);
-  app.post('/api/v1/rides/:rideOfferId/requests', rideOfferAuth.rideOfferExist, rideOffersController.joinRide);
-  app.put('/api/v1/rides/:rideOfferId', rideOfferAuth.rideOfferExist, rideOffersController.updateRideOfferDetails);
-  app.delete('/api/v1/rides/:rideOfferId', rideOfferAuth.rideOfferExist, rideOffersController.deleteRideOffer);
+  app.post('/api/v1/auth/signup', user.signup);
+  app.post('/api/v1/auth/signin', user.signIn);
+  // app.get('/api/v1/rides', rideOffers.getAllRideOffers);
+  // app.get('/api/v1/rides/:rideOfferId', rideOffers.getSpecificRideOffer);
+  app.post('/api/v1/rides', rideOffers.createRide);
+  // app.post('/api/v1/rides/:rideOfferId/requests', rideOffers.joinRide);
+  // app.put('/api/v1/rides/:rideOfferId', rideOffers.updateRideOfferDetails);
+  // app.delete('/api/v1/rides/:rideOfferId', rideOffers.deleteRideOffer);
 };
 
 export default routes;
