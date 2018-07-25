a89850dca6cce015b264665f4561fdee42574882
okoro emeka <solomonokoro056@gmail.com>
2018-07-25 01:25:19 +0100

    fix heroku

--- server/models/config.js
+++ server/models/config.js
@@ -2,15 +2,25 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+let connectionString;
+const {DB_NAME, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_HOST, DB_URL, NODE_ENV, TEST_DB} = process.env;
 const config = {
-  user: 'postgres',
-  database: 'postgres',
-  password: 'null',
-  port: 5432,
+  user: DB_USERNAME,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
   max: 10,
   idleTimeoutMillis: 3000,
 };
-// const connectionString = process.env.DB_URL;
-const dbConnection = new pg.Pool(config);
+
+if (NODE_ENV === 'test') {
+	connectionString = TEST_DB;
+	ssl = true;
+} else {
+	connectionString = process.env.DATABASE_URL;
+	ssl = false;
+}
+// const connectionString = DB_URL;
+const dbConnection = new pg.Pool({ connectionString, ssl });
 
 export default dbConnection;
